d6748f6889c3d26b612588a407003fb40788b527
Ruslan Nakhmedov <ruslan-nahmedov@mail.ru>
2026-08-19 19:41:28 +0300

    add controllers

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from catalog.views import contact_view, submit_form
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("catalog/", include("catalog.urls", namespace="catalog")),
+    path("contacts/", contact_view, name="contacts"),
+    path("submit_form/", submit_form, name="submit_form"),
 ]
