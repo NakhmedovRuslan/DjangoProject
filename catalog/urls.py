a9053c711498556d17d87106243f32b6d6671c24
Ruslan Nakhmedov <ruslan-nahmedov@mail.ru>
2026-08-19 19:42:18 +0300

    add README.MD update urls.py

---
+++ catalog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from catalog.apps import CatalogConfig
+from catalog.views import main_view, contact_view, submit_form
+
+app_name = CatalogConfig.name
+
+urlpatterns = [
+    path("main", main_view, name="main"),
+    path("contacts", contact_view, name="contacts"),
+    path("submit_form", submit_form, name="submit_form"),
+]
