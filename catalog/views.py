b8a6b21257ed3c1ee1d4ceb76209c55d1cbfba55
Ruslan Nakhmedov <ruslan-nahmedov@mail.ru>
2026-08-21 17:38:01 +0300

    update urls/views

--- catalog/views.py
+++ catalog/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def main_view(request):
+def home_view(request):
     return render(request, 'main.html')
 
 def contact_view(request):
